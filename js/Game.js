ce50e2a66b4cd9ce7ef95114a9feb352e858e983
Dariusz Okonski <darek200180@gmail.com>
2021-03-22 00:55:35 +0000

    add ship moving

--- js/Game.js
+++ js/Game.js
@@ -1,12 +1,19 @@
-import { Spaceship } from './Spaceship';
+import { Spaceship } from './Spaceship.js';
 
 class Game {
+    #htmlElements = {
+        spaceship: document.querySelector('[data-spaceship]')
+    }
+    #ship = new Spaceship(this.#htmlElements.spaceship)    
+    
     init() {
-        console.log('init')
+        this.#ship.init()
+        console.log('working')
     }   
 }
 
 window.onload = function() {
     const game = new Game();
     game.init();
+    console.log(game)
 }
